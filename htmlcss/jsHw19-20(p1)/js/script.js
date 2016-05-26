4246ef2c4752ac62538580b8d3d3543e2f9f6db9
iMelnykV <trxo@mail.ua>
2016-05-26 17:39:30 +0300

    Initial commit jsHw19-20

---
+++ htmlcss/jsHw19-20(p1)/js/script.js
@@ -0,0 +1,21 @@
+$(function () {
+	$('.flexslider').flexslider({
+		animation: "slide",
+		controlsContainer: $(".custom-controls-container"),
+	});
+
+	function close_accordion_section() {
+		$('.accordion .accordion-section-title').removeClass('active');
+		$('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+	};
+
+	$('.accordion-section-tittle').click(function(e) {
+		var currentAttrValue = $(this).attr('href');
+		close_accordion_section();
+
+		$(this).addClass('active');
+		$('.accordion-section-tittle').not($(this)).removeClass('active');
+		$('.accordion ' + currentAttrValue).slideDown(300).addClass('open');
+		e.preventDefault();
+	});
+})
