8b46292ba316f0d25d13746c03ab73429962b72f
iMelnykV <trxo@mail.ua>
2016-05-31 13:52:53 +0300

    Initial commit jsHw19-20p1 new

--- htmlcss/jsHw19-20(p1)/gruntfile.js
+++ htmlcss/jsHw19-20(p1)/gruntfile.js
@@ -1,27 +1,35 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    concat: {
+      dist: {
+        src: ['styles/variables.scss', 'styles/reset.scss', 'styles/style.scss'],
+        dest: 'styles/main.scss'
+      }
+    },
     sass: {
       dist: {
         files: [{
           expand: true,
-          cwd: 'css',
-          src: ['scss/*.scss'],
-          dest: 'css',
+          cwd: 'styles',
+          src: ['main.scss'],
+          dest: 'styles',
           ext: '.css'
         }]
       }
     },
     watch: {
       sass: {
-        files: ['css/scss/*.scss'],
-        tasks: ['sass'],
-      }
+        //We watch and complete sass files as normal but don't live reload
+        files: ['styles/*.scss'],
+        tasks: ['concat', 'sass']
+      },
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('default', ['concat', 'sass']);
 };
