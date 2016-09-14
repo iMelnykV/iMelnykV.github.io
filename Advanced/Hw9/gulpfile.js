06405ed0215fcaddd8b2b16ae839e1e5cc0f984c
iMelnykV <victor.z.melnik@gmail.com>
2016-09-15 00:37:53 +0300

    Add new homeWork9

---
+++ Advanced/Hw9/gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+
+gulp.task('sass', function () {
+  return gulp.src('styles/sass/main.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('styles/css'));
+});
+
+// Задача по умолчанию, вызывается запуском `gulp`
+gulp.task('default', ['sass', 'watch']);
+
+gulp.task('watch', ['sass'], function () {
+  gulp.watch('styles/sass/*.scss', ['sass']);
+});
