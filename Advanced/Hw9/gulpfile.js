438ab2a945c8c86c0da301169067887dc578451f
iMelnykV <victor.z.melnik@gmail.com>
2016-09-16 23:46:34 +0300

    Add new Hw9

--- Advanced/Hw9/gulpfile.js
+++ Advanced/Hw9/gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 
+//gulp-sass
 gulp.task('sass', function () {
   return gulp.src('styles/sass/main.scss')
     .pipe(sass.sync().on('error', sass.logError))
@@ -11,6 +12,7 @@ gulp.task('sass', function () {
 // Задача по умолчанию, вызывается запуском `gulp`
 gulp.task('default', ['sass', 'watch']);
 
+//gulp-watch
 gulp.task('watch', ['sass'], function () {
   gulp.watch('styles/sass/*.scss', ['sass']);
 });
